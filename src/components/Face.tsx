55c6156ce1ec4d7441fb4c8cda23b4b91328f621
angel6497 <angel.ortizregules@mail.mcgill.ca>
2024-05-16 20:15:26 -0700

    Fix flex tile ordering

--- src/components/Face.tsx
+++ src/components/Face.tsx
@@ -10,46 +10,19 @@ const Face: FC<FaceProps> = ({ tileColors }) => {
   return (
     <div className="face">
       <div className="row">
-        <div
-          className="rubik-tile"
-          style={{ backgroundColor: tileColors[0] }}
-        />
-        <div
-          className="rubik-tile"
-          style={{ backgroundColor: tileColors[1] }}
-        />
-        <div
-          className="rubik-tile"
-          style={{ backgroundColor: tileColors[2] }}
-        />
+        <div className="tile" style={{ backgroundColor: tileColors[0] }} />
+        <div className="tile" style={{ backgroundColor: tileColors[1] }} />
+        <div className="tile" style={{ backgroundColor: tileColors[2] }} />
       </div>
       <div className="row">
-        <div
-          className="rubik-tile"
-          style={{ backgroundColor: tileColors[3] }}
-        />
-        <div
-          className="rubik-tile"
-          style={{ backgroundColor: tileColors[4] }}
-        />
-        <div
-          className="rubik-tile"
-          style={{ backgroundColor: tileColors[5] }}
-        />
+        <div className="tile" style={{ backgroundColor: tileColors[3] }} />
+        <div className="tile" style={{ backgroundColor: tileColors[4] }} />
+        <div className="tile" style={{ backgroundColor: tileColors[5] }} />
       </div>
       <div className="row">
-        <div
-          className="rubik-tile"
-          style={{ backgroundColor: tileColors[6] }}
-        />
-        <div
-          className="rubik-tile"
-          style={{ backgroundColor: tileColors[7] }}
-        />
-        <div
-          className="rubik-tile"
-          style={{ backgroundColor: tileColors[8] }}
-        />
+        <div className="tile" style={{ backgroundColor: tileColors[6] }} />
+        <div className="tile" style={{ backgroundColor: tileColors[7] }} />
+        <div className="tile" style={{ backgroundColor: tileColors[8] }} />
       </div>
     </div>
   );
